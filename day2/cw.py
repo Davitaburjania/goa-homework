be23b0cf059f149217f41afc60ed756764eb3bbf
Davitaburjania <aburjaniadaviti@gmail.com>
2025-10-08 18:27:47 +0400

    second commit

---
+++ day2/cw.py
@@ -0,0 +1,18 @@
+print(59+84)
+print(33+34)
+print(46+73)
+
+
+print(34-65)
+print(64-29)
+print(43-12)
+
+
+print(23*3)
+print(2*7)
+print(37*8)
+
+
+print(56/ 4)
+print(30/3)
+print(42/6)
---
+++ day3/homework/hww.py
@@ -0,0 +1,8 @@
+name="house"
+print(name)
+ 
+name="city" 
+print(name)
+
+name="paper"
+print(name)
