59cf904317fe209815b316f547b4e3e7d2a17923
Davitaburjania <aburjaniadaviti@gmail.com>
2025-10-24 23:34:39 +0400

    second commit

--- day7/hw.py
+++ day7/hw.py
@@ -1,8 +1,12 @@
 #input-ი არის პროცესი, როდესაც კომპიუტერში შედის რაღაც ინფორმაცია
 #output-ი არის პროცესი, როდესაც კომპიუტერიდან გამოდის რაღაც ინფორმაცია 
+
+
+
 name= input("enter your name: ")
 age= input("enter your age: ")
 print(f"hello your name is {name}, and your age is {age}")
+
 
 
 x1=int(input("I cifri: "))
@@ -10,4 +14,4 @@ x2=int(input("II cifri: "))
 x3=int(input("III cifri: "))
 x4=int(input("IV cifri: "))
 x5=int(input("V cifri: "))
-print("sashvalo aritmetikuli", (x1+x2+x3+x4+x5)/5)
+print("sashvalo aritmetikuli:", (x1+x2+x3+x4+x5)/5)
