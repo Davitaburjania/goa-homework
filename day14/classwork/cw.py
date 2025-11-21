46a05ca1c7881dde98c3ca781ab0c9f4bb2e7e0f
Davitaburjania <aburjaniadaviti@gmail.com>
2025-11-22 02:08:22 +0400

    second commit

---
+++ day14/classwork/cw.py
@@ -0,0 +1,10 @@
+for i in range(100):
+    print(i)
+
+for i in range(101):
+    print(i)
+
+
+for i in range(5):
+    number=int(input("შეიყვანე რიცხვი "))
+    print(number ** 2)
---
+++ day14/homework/hw6.py
@@ -0,0 +1,8 @@
+total=0
+for i in range(5):
+    number=float(input("შეიყვანე რიცხვი "))
+total += number 
+
+average = total/5 
+
+print("საშვალო არითმეტიკული: " , average)
