7ebdb1ae10245a268b9fc0371fa184659136c691
Davitaburjania <aburjaniadaviti@gmail.com>
2026-01-23 17:45:07 +0400

    second commit

---
+++ day29/classwork/cw.py
@@ -0,0 +1,21 @@
+nums = [10, 20, 30, 40, 50, 60, 70]
+
+print(nums[0])
+
+print(nums[-1])
+
+print(nums[0:3])
+
+print(nums[-2:])
+
+print(nums[1:6])
+
+print(nums[::-1])
+
+print(nums[1])
+
+print(nums[::2])
+
+print(nums[3])
+
+print(nums[0:3])
